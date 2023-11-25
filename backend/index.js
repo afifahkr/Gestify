4713f35974f47d98fb5c4ef7896ce5f603fe2888
Afifahkr <75968800+afifahkr@users.noreply.github.com>
2023-11-25 18:09:19 +0700

    add refresh token, verify token, login, logout,etc

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,10 @@
 import express from "express"
 import dotenv from "dotenv"
+import cookieParser from "cookie-parser"
+import cors from "cors"
 import db from "./config/database.js"
 import router from "./routes/index.js"
-//import Users from "./models/UserModel.js" (komen setelah tabel berhasil digenerate)
+//import Users from "./models/UserModel.js" // (komen setelah tabel berhasil digenerate)
 dotenv.config()
 const app = express()
 
@@ -10,12 +12,14 @@ try {
     await db.authenticate();
     console.log('Database Connected')
         //Jika kita tida punya kolom Users di db kita, sequelize akan otomatis mengenerate
-        // await Users.sync(); (di komen/hapus setelah tabel digenerate)
+        //await Users.sync(); //(di komen/hapus setelah tabel digenerate)
 
 } catch (error) {
     console.error(error)
 }
 
+app.use(cors({ credentials: true, origin: 'http://localhost:3000' })) //kalo pake react js port 3000
+app.use(cookieParser())
 app.use(express.json()) //agar bisa menerima data dalam format json
 app.use(router) //middleware
 
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,13 +2,21 @@ import express from "express"
 //menangani permintaan (request) untuk mendapatkan data pengguna.
 import { getUsers } from "../controllers/Users.js";
 import { Register } from "../controllers/Users.js";
+import { Login } from "../controllers/Users.js";
+import { Logout } from "../controllers/Users.js";
+import { verifyToken } from "../middleware/VerifyToken.js";
+import { refreshToken } from "../controllers/RefreshToken.js";
 
 //Membuat objek router dari Express.
 const router = express.Router();
 
 //Mendefinisikan rute HTTP GET pada path /users. Ketika permintaan GET diterima pada path ini, fungsi getUsers dari controller akan dijalankan.
-router.get('/users', getUsers)
+router.get('/users', verifyToken, getUsers)
 router.post('/users', Register)
+router.post('/login', Login)
+router.get('/token', refreshToken)
+router.delete('/logout', Logout)
+
 
 //Mengeskpor objek router agar dapat digunakan di file lain dalam proyek. 
 export default router
